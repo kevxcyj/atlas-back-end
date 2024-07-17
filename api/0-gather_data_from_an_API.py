ec38042cae3b5773dab1f42384221e4568fdce95
kevxcyj <kevcyjunior@gmail.com>
2024-07-16 23:34:11 -0500

    Changed 0 and 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,41 +5,31 @@ import requests
 import sys
 
 
-def get_employeeprogress(employee_id):
-    url = "https://jsonplaceholder.typicode.com/"
+import requests
+from sys import argv
 
-    user_response = requests.get("{}/users/{}".format(url, employee_id))
-    if user_response.status_code != 200:
-        print("Error fetching user with ID {}".format(employee_id))
-        return
 
-    data = user_response.json()
-    employeename = data.get('name')
-    todolist_request = requests.get("{}/todos?userId={}"
-                                  .format(url, employee_id))
-    
-    if todolist_request.status_code != 200:
-        print("Error fetching TODO list for user with ID {}"
-              .format(employee_id))
+def get_employee_todos(employee_id):
+    """ Employee ID """
 
-    todolist_data = todolist_request.json()
-    total_tasks = len(todolist_data)
-    complete_tasks = [task for task in todolist_data if task.get('completed')]
-    total_number_of_tasks = len(complete_tasks)
+    site = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(site + "users/{}".format(employee_id))
+    todo = requests.get(site + "todos?userId={}".format(employee_id))
+
+    users = user.json()
+    names = users.get("name")
+    todos = todo.json()
+
+    completed = [i for i in todos if i.get("completed")]
+    all_todos = len(todos)
+    done_todos = len(completed)
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(employeename, total_number_of_tasks, total_tasks))
+          .format(names, done_todos, all_todos))
 
-    for task in complete_tasks:
-        print(f"\t {task.get('title')}")
+    for i in completed:
+        print("\t {}".format(i.get("title")))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employeeprogress(employee_id)
-        except ValueError:
-            print("Please provide a valid integer as employee ID")
+    get_employee_todos(employee_id=argv[1])
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,59 +3,32 @@
 
 import csv
 import requests
-import sys
+from sys import argv
 
 
-def get_employeeprogress(employee_id):
-    url = "https://jsonplaceholder.typicode.com"
+def get_employee_todos(employee_id):
+    """ Employee ID """
 
-    user_response = requests.get("{}/users/{}".format(url, employee_id))
-    if user_response.status_code != 200:
-        print("Error fetching user with ID {}".format(employee_id))
-        return
+    site = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(site + "users/{}".format(employee_id))
+    todo = requests.get(site + "todos?userId={}".format(employee_id))
 
-    data = user_response.json()
-    username = data.get('username')
-    print("User ID: {}, Username: {}".format(employee_id, username))
+    users = user.json()
+    usernames = users.get("username")
+    todos = todo.json()
+    csv_file_name = "{}.csv".format(employee_id)
 
-    todos_request = requests.get("{}/todos?userId={}"
-                                  .format(url, employee_id))
-    if todos_request.status_code != 200:
-        print("Error fetching TODO list for user with ID {}"
-              .format(employee_id))
-        return
+    completed = [i for i in todos if i.get("completed")]
 
-    todolist_data = todos_request.json()
-    total_tasks = len(todolist_data)
-    complete_tasks = [task for task in todolist_data if task.get('completed')]
-    total_number_of_tasks = len(complete_tasks)
+    for i in completed:
+        print("\t {}".format(i.get("title")))
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(username, total_number_of_tasks, total_tasks))
-
-    for task in complete_tasks:
-        print("\t {}".format(task.get('title')))
-
-    with open("{}.csv".format(employee_id), mode='w', newline='') as file:
+    with open(csv_file_name, mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for task in todolist_data:
-            writer.writerow([employee_id, username, task.get('completed'),
-                             task.get('title')])
-
-    with open("{}.csv".format(employee_id), mode='r') as file:
-        reader = csv.reader(file)
-        csv_task  = sum(1 for row in reader)
-        if csv_task != total_tasks:
-            print("Warning: Number of tasks in CSV ({}) does not match the "
-                  "expected count ({}).".format(csv_task, total_tasks))
+        for i in todos:
+            writer.writerow([employee_id, usernames,
+                             i.get('completed'), i.get('title')])
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employeeprogress(employee_id)
-        except ValueError:
-            print("Please provide a valid integer as employee ID")
+    get_employee_todos(employee_id=argv[1])
