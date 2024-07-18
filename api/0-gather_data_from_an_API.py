ec7abb54bf86663b73240a0c859ee89e2bbe5eea
kevxcyj <kevcyjunior@gmail.com>
2024-07-17 22:22:04 -0500

    Pycode

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -38,7 +38,7 @@ def fetch_employee_todo_progress(employee_id):
 
     # Prepare and print task list
     print(f"Employee {employee_name} is done with tasks 
-        ({len(completed_todos)}/{len(todos_data)}):")
+                    ({len(completed_todos)}/{len(todos_data)}):")
     for todo in completed_todos:
         print(f"\t{todo['title']}")
 
