276266438d31fd958b6e426e5fdb4205051aa0e3
kevxcyj <kevcyjunior@gmail.com>
2024-07-17 22:45:09 -0500

    pycode

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,32 +3,66 @@
 
 import csv
 import requests
-from sys import argv
+import sys
 
 
-def get_employee_todos(employee_id):
-    """ Employee ID """
+USERS_URL = "https://jsonplaceholder.typicode.com/users/"
+TODOS_URL = "https://jsonplaceholder.typicode.com/todos?userId="
 
-    site = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(site + "users/{}".format(employee_id))
-    todo = requests.get(site + "todos?userId={}".format(employee_id))
 
-    users = user.json()
-    usernames = users.get("username")
-    todos = todo.json()
-    csv_file_name = "{}.csv".format(employee_id)
+def get_csv_export(employee_id):
+    """
+    Fetches a user and their todo list from the JSONPlaceholder API and exports
+    the data to a CSV file named after the user's ID.
 
-    completed = [i for i in todos if i.get("completed")]
+    Parameters:
+    - employee_id: The ID of the employee to retrieve information for.
+    """
+    user_response = requests.get(f"{USERS_URL}{employee_id}")
+    if user_response.status_code != 200:
+        print("Failed to retrieve employee information.")
+        return
 
-    for i in completed:
-        print("\t {}".format(i.get("title")))
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
+    if not employee_name:
+        print("Employee not found.")
+        return
 
-    with open(csv_file_name, mode='w', newline='') as file:
+    username = user_data.get('username')
+    if not username:
+        print("Employee does not have a username set.")
+        return
+
+    todos_response = requests.get(f"{TODOS_URL}{employee_id}")
+    if todos_response.status_code != 200:
+        print("Failed to retrieve TODO list. Please check your parameters.")
+        return
+
+    todos = todos_response.json()
+    csv_file_name = f"{employee_id}.csv"
+
+    # Prepare data for csv [[List of Lists]]
+    csv_data = [[]]
+    for item in todos:
+        csv_data.append(
+            [employee_id, username, item["completed"], item["title"]])
+
+    # Write csv_data to .csv file
+    with open(f"{employee_id}.csv", mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for i in todos:
-            writer.writerow([employee_id, usernames,
-                             i.get('completed'), i.get('title')])
+        writer.writerows(csv_data)
 
 
 if __name__ == "__main__":
-    get_employee_todos(employee_id=argv[1])
+    if len(sys.argv) != 2:
+        print("Please enter the employee ID.")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    get_csv_export(employee_id)
