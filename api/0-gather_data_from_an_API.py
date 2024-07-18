bfa80c05fd451685796f71ae93257d4e1f4849b0
kevxcyj <kevcyjunior@gmail.com>
2024-07-17 22:26:02 -0500

    pycode

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -29,8 +29,7 @@ def fetch_employee_todo_progress(employee_id):
     (f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
 
     if todos_response.status_code != 200:
-        print (f"Error fetching TODO list for employee with ID {employee_id}. 
-               Status code: {todos_response.status_code}")
+        print (f"Error fetching TODO list for employee with ID {employee_id}. Status code: {todos_response.status_code}")
         sys.exit(1)
 
     todos_data = todos_response.json()
