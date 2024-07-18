34a2dcac51ab6b0e113f9a12779e5a51bd547dd2
kevxcyj <kevcyjunior@gmail.com>
2024-07-17 22:17:25 -0500

    Fixed pycode

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,27 +10,29 @@ def fetch_employee_todo_progress(employee_id):
     Fetches and displays the TODO list progress for a given employee ID.
     """
 
-    name_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    
+    name_response = requests.get(f"https://jsonplaceholder.typicode.com
+                                    /users/{employee_id}")
+
     if name_response.status_code != 200:
-        print(f"Error fetching employee with ID {employee_id}. Status code: {name_response.status_code}")
+        print(f"Error fetching employee with ID {employee_id}. 
+                Status code: {name_response.status_code}")
         sys.exit(1)
-    
+
     name_data = name_response.json()
 
     if not name_data:
         print(f"Employee with ID {employee_id} not found.")
         sys.exit(1)
-    
+
     employee_name = name_data['name']
 
-    # Fetch all todos for the employee
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-    
+    todos_response = requests.get(f"https://jsonplaceholder.typicode.com
+                                    /todos?userId={employee_id}")
+
     if todos_response.status_code != 200:
         print(f"Error fetching TODO list for employee with ID {employee_id}. Status code: {todos_response.status_code}")
         sys.exit(1)
-    
+
     todos_data = todos_response.json()
 
     # Filter completed todos
